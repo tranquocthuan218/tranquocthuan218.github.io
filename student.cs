66d13dca7d9253ef21756ad502a2a492b40da36b
tranquocthuan218 <71358397+tranquocthuan218@users.noreply.github.com>
2021-04-10 20:05:18 +0700

    Update student.cs

--- student.cs
+++ student.cs
@@ -1,1 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 
+namespace WpfApp2
+{
+
+    class Student
+    {
+        // 1. Định nghĩa trường Name: 
+        private string Name;
+
+        // 2. Định nghĩa thuộc tính StudentName từ trường Name
+        public string StudentName
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public Student()
+        {}
+        // 3. Định nghĩa phương thức khởi tạo Student(string stName)
+        public Student(string stName)
+        {
+            Name = stName;
+        }
+        // 4. Định nghĩa phương thức Talk() giới thiệu tên của sinh viện thông qua MessageBox
+
+        public virtual void Talk()
+        {
+            MessageBox.Show(Name);
+        }
+
+    }
+}
